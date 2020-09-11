74f242691992386e04178bfdca40238f4b101aa9
Suphachai Sriwised <suphachai.sriwised.o@gmail.com>
2020-09-11 22:20:51 +0700

    make reuse code by include head.ejs

--- app/routes/index.js
+++ app/routes/index.js
@@ -6,4 +6,14 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/add',(req,res, next)=>{
+  res.send('Adding data');
+})
+router.get('/delete',(req,res, next)=>{
+  res.send('Delete data');
+})
+router.get('/update',(req,res, next)=>{
+  res.send('Update data');
+})
+
 module.exports = router;
